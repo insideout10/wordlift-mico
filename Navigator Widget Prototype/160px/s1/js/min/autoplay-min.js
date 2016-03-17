df9ded6b757628a1499bca8cb7b11c0a5a187f17
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2016-03-17 10:30:08 +0100

    Initial commit

---
+++ Navigator Widget Prototype/160px/s1/js/min/autoplay-min.js
@@ -0,0 +1,1 @@
+$(function(){"video".playbackRate=.5}),$(function(){$("video").on("mouseenter",function(){this.paused&&(this.play().playbackRate=.5)}).on("mouseleave",function(){this.paused||this.pause()})});
